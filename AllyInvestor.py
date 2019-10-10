00c1a2f2aa9ade5742ce3603c9629a07a0cc33db
Zack Ulam <zackulam@Zacks-MacBook-Pro.local>
2019-10-10 18:12:11 -0400

    adding test

--- AllyInvestor.py
+++ AllyInvestor.py
@@ -1,6 +1,12 @@
 import requests
 
-url = '/v1/market/ext/quotes.xml?symbols=aapl'
-params = {'symbols':'AAPL'}
-r = requests.get(url = url, params=params)
-print('success')
+ticker_list = []
+company_file = open('nasdaqlisted.txt')
+for line in company_file:
+    ticker_list.append(line.strip('\n'))
+    
+print("ay")
+# url = '/v1/market/ext/quotes.xml?symbols=aapl'
+# params = {'symbols':'AAPL'}
+# r = requests.get(url = url, params=params)
+# print('success')
