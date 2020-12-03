2728ccb61b8965c52619dc978678ba56bfa3a297
zackulam <zulam@kent.edu>
2020-12-02 20:54:50 -0500

    news scanner added

--- trader_new.py
+++ trader_new.py
@@ -10,30 +10,33 @@ import Stock
 from datetime import datetime, timedelta
 from dateutil.parser import parse
 
+email_queue = []
+email_sent = []
+
 def checkNews(ticker):
-    tod = datetime.now()
-    d = timedelta(2)
-    lim = tod - d
-    newsCount = 0
-    newsTarget = 2
-    newsUrl = 'https://api.tradeking.com/v1/market/news/search.json?symbols='
-    temp_news = newsUrl + ticker + '&maxhits=10'
-    try:
-        res = auth.get(temp_news)
-        json_news = res.json()
-        articles = json_news['response']['articles']
-        for article in articles['article']:
-            if parse(article['date']) >= lim:
-                newsCount += 1
-        if newsCount >= newsTarget:
-            return True
-        else: 
-            return False
-    except Exception as e:
-        print(e) 
+    if ticker not in email_sent:
+        tod = datetime.now()
+        d = timedelta(1)
+        lim = tod - d
+        newsUrl = 'https://api.tradeking.com/v1/market/news/search.json?symbols='
+        temp_news = newsUrl + ticker + '&maxhits=10'
+        try:
+            res = auth.get(temp_news)
+            json_news = res.json()
+            articles = json_news['response']['articles']
+            for article in articles['article']:
+                if parse(article['date']) >= lim:
+                    email_sent.append(ticker)
+                    return article['date'] + '\n' + article['headline'] 
+            return 'nothing'
+        except Exception as e:
+            print(e) 
+            return 'nothing'
+    else:
+        return 'nothing'
 
 def sendEmail(message):
-    if datetime.now().hour >= 8 and datetime.now().hour <= 20:
+    if datetime.now().hour >= 6 and datetime.now().hour <= 20:
         try:
             smtp_server = "smtp.gmail.com"
             port = 587
@@ -53,6 +56,22 @@ marketClockUrl = 'https://api.tradeking.com/v1/market/clock.json'
 rate_lim = .05
 running = True
 while running:
+    #TEST
+    # master_list = []
+    # try:
+    #     auth = OAuth1Session(
+    #         cfg.key['consumer_key'],
+    #         cfg.key['consumer_secret'],
+    #         cfg.key['oauth_token'],
+    #         cfg.key['oauth_token_secret'])
+    #     res = auth.get(marketClockUrl)
+    #     mas_file = open(cfg.file['master_file'])
+    #     for line in mas_file:
+    #         master_list.append(line.strip())
+    # except Exception as e:
+    #     print(e)
+    #ENDTEST
+    
     #clock
     time.sleep(1)
     try:
@@ -96,8 +115,6 @@ while running:
     
     # filter out expensive stocks only when market is closed
     if clockJson == 'close' and datetime.now().hour <= 5:
-        ticker_list_condensed.clear()
-        exclude.clear()
         # penny_file = open(cfg.file['penny_list'])
         # for line in penny_file:
         #     ticker = line.strip()
@@ -114,26 +131,65 @@ while running:
         #             ctr += 1
         #     except Exception as error:
         #         print(error)
-        for ticker in ticker_list:
-            temp_url = url + ticker
-            try:
-                time.sleep(1)
-                r = auth.get(temp_url)
-                json_result = r.json()
-                avg_str = json_result['response']['quotes']['quote']['adp_100']
-                avg = float(avg_str) if avg_str != '' else 0
-                if (avg <= price_max and avg != 0 and avg >= price_min) or (avg <= non_penny_max and avg >= non_penny_min):
-                    ticker_list_condensed.append(ticker)
-                    print(ticker)
-                    ctr += 1
-            except Exception as error:
-                print(error)
-        f = open("tickers_" + str(datetime.now().month) + "-" + str(datetime.now().day) + "-" + str(datetime.now().year) + ".txt", "w")
-        for ticker in ticker_list_condensed:
-            f.write(ticker + '\n')
-        f.close()
-        print('open cycle done')
+        if len(ticker_list_condensed) == 0:
+            for ticker in ticker_list:
+                temp_url = url + ticker
+                try:
+                    time.sleep(1)
+                    r = auth.get(temp_url)
+                    json_result = r.json()
+                    avg_str = json_result['response']['quotes']['quote']['adp_100']
+                    avg = float(avg_str) if avg_str != '' else 0
+                    if (avg <= price_max and avg != 0 and avg >= price_min) or (avg <= non_penny_max and avg >= non_penny_min):
+                        ticker_list_condensed.append(ticker)
+                        print(ticker)
+                        ctr += 1
+                except Exception as error:
+                    print(error)
+            f = open(cfg.file['master_file'], "w")
+            for ticker in ticker_list_condensed:
+                f.write(ticker + '\n')
+            f.close()
         
+        for ticker in ticker_list_condensed:
+            headline = checkNews(ticker)
+            message = '\n'
+            if headline != 'nothing':
+                message += ticker + ': ' + headline + '\n'
+                email_queue.append(message)
+        print('close cycle done')
+    elif clockJson == 'close' and datetime.now().hour > 5 and datetime.now().hour < 22:
+        if len(email_queue) > 0:
+            for email in email_queue:
+                try:
+                    sendEmail(email)
+                    time.sleep(1)
+                    email_queue.remove(email)
+                except Exception as e:
+                    print(e)
+        for ticker in ticker_list_condensed:
+            headline = checkNews(ticker)
+            message = '\n'
+            if headline != 'nothing':
+                message += ticker + ': ' + headline
+                try:
+                    sendEmail(message)
+                    time.sleep(1)
+                except Exception as e:
+                    print(e)
+        print('close cycle done')
+    elif clockJson == 'close' and (datetime.now().hour >= 22 or datetime.now().hour <= 5):
+        running = False
+        print('close cycle done')
+
+    if clockJson == 'pre':
+        for ticker in ticker_list_condensed:
+            headline = checkNews(ticker)
+            message = '\n'
+            if headline != 'nothing':
+                message += '\n' + ticker + ': ' + headline
+                sendEmail(message)
+        print('pre cycle done')
     # market has opened
     if clockJson == 'open':
         master_list = []
@@ -164,9 +220,15 @@ while running:
                 if low == 0:
                     low = .001
                 rate_from_low = (ask - low) / low
-                if rate_from_low < rate_lim and low != 0 and rate_from_low != -1:
+                headline = checkNews(ticker)
+                approach_low = (rate_from_low < rate_lim and low != 0 and rate_from_low != -1)
+                message = '\n'
+                if approach_low or headline != 'nothing':
                     # send email to buy stock
-                    message = 'Buy ' + ticker + ' at ' + str(ask) + ' (' \
+                    if headline != 'nothing':
+                        message += '\n' + ticker + ': ' + headline
+                    if approach_low:
+                        message += '\n' + 'Buy ' + ticker + ' at ' + str(ask) + ' (' \
                             + str(round(rate_from_low, 4) * 100) + '% from 52 week low)'
                     stocks_bought[ticker] = ask
                     sendEmail(message)
@@ -176,9 +238,13 @@ while running:
                 if hi == 0 or ask <= 5:
                     continue
                 rate_from_high = (hi - ask) / hi
-                if rate_from_high < rate_lim and hi != 0 and rate_from_high != -1:
+                approach_high = (rate_from_high < rate_lim and hi != 0 and rate_from_high != -1)
+                if approach_high or headline != 'nothing':
                     # send email to buy stock
-                    message = 'Short ' + ticker + ' at ' + str(ask) + ' (' \
+                    if headline != 'nothing':
+                        message += '\n' + ticker + ': ' + headline + '\n'
+                    if approach_high:
+                        message += '\n' + 'Short ' + ticker + ' at ' + str(ask) + ' (' \
                             + str(round(rate_from_high, 4) * 100) + '% from 52 week high)'
                     stocks_bought[ticker] = ask
                     sendEmail(message)
@@ -187,7 +253,4 @@ while running:
         except Exception as error:
             print('ERROR: ', error)
         print('open cycle done')
-    if clockJson == 'after':
-        running = False
-        print('after cycle done')
 print("complete") 
